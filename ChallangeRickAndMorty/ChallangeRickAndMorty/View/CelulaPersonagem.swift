d09726b6f7a1db72b5ba29778ab206f358c80c99
Ana Pontes <analopesps20@gmail.com>
2023-06-24 14:31:35 -0300

    feat - conectando files

--- ChallangeRickAndMorty/ChallangeRickAndMorty/View/CelulaPersonagem.swift
+++ ChallangeRickAndMorty/ChallangeRickAndMorty/View/CelulaPersonagem.swift
@@ -9,12 +9,19 @@ import UIKit
 
 class CelulaPersonagem: UIView {
 
-    
     var nomePersonagem: UILabel = {
         let nome = UILabel()
         nome.text = "Morty"
-        nome.textColor = .black
+        nome.textColor = .white
         return nome
+    }()
+    
+    var posterPersonagem: UIImageView = {
+        let poster = UIImageView()
+        poster.image = UIImage(named: "rick")
+        poster.contentMode = .scaleToFill
+        return poster
+    
     }()
     
     override init(frame: CGRect) {
@@ -27,15 +34,29 @@ class CelulaPersonagem: UIView {
     }
     func setupCell() {
         
+        
+        
         addSubview(nomePersonagem)
-    
+        addSubview(posterPersonagem)
+        
         nomePersonagem.translatesAutoresizingMaskIntoConstraints = false
+        posterPersonagem.translatesAutoresizingMaskIntoConstraints = false
+
 
         NSLayoutConstraint.activate([
         nomePersonagem.topAnchor.constraint(equalTo: topAnchor),
-        nomePersonagem.centerXAnchor.constraint(equalTo: centerXAnchor),
-        nomePersonagem.centerYAnchor.constraint(equalTo: centerYAnchor),
-        nomePersonagem.bottomAnchor.constraint(equalTo: bottomAnchor)
+      //nomePersonagem.centerXAnchor.constraint(equalTo: centerXAnchor),
+        nomePersonagem.bottomAnchor.constraint(equalTo: bottomAnchor),
+       //nomePersonagem.centerYAnchor.constraint(equalTo: centerYAnchor),
+        nomePersonagem.trailingAnchor.constraint(equalTo: trailingAnchor),
+        nomePersonagem.leadingAnchor.constraint(equalTo: posterPersonagem.trailingAnchor),
+        
+        posterPersonagem.widthAnchor.constraint(equalToConstant: 150),
+        posterPersonagem.leadingAnchor.constraint(equalTo: leadingAnchor),
+        //posterPersonagem.trailingAnchor.constraint(equalTo:trailingAnchor),
+        posterPersonagem.heightAnchor.constraint(equalToConstant: 150),
+        posterPersonagem.topAnchor.constraint(equalTo: topAnchor),
+        
         
         ])
         
--- ChallangeRickAndMorty/ChallangeRickAndMorty/View/HomeCell.swift
+++ ChallangeRickAndMorty/ChallangeRickAndMorty/View/HomeCell.swift
@@ -9,23 +9,24 @@ import UIKit
 
 class HomeCell: UITableViewCell {
     
-    var personagem = CelulaPersonagem()
     
-    var celulaPersonagem: UIView = {
-       var celula = CelulaPersonagem()
-        return celula
-    }()
+    var celula = CelulaPersonagem()
+    
+//    var celulaPersonagem: UIView = {
+//       var celula = CelulaPersonagem()
+//        return celula
+//    }()
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        celulaPersonagem.backgroundColor = .darkGray
-        celulaPersonagem.translatesAutoresizingMaskIntoConstraints = false
-        celulaPersonagem.layer.masksToBounds = true
-        celulaPersonagem.layer.cornerRadius = 20
-
+//        celulaPersonagem.backgroundColor = .darkGray
+//        celulaPersonagem.translatesAutoresizingMaskIntoConstraints = false
+//        celulaPersonagem.layer.masksToBounds = true
+//        celulaPersonagem.layer.cornerRadius = 20
+        
+        
         
         setupCell()
-        //celulaPersonagem = personagem        
     }
     
     required init?(coder: NSCoder) {
@@ -33,18 +34,23 @@ class HomeCell: UITableViewCell {
     }
     
     func setupCell() {
+        addSubview(contentView)
+        contentView.addSubview(celula)
         
-        addSubview(celulaPersonagem)
+      //  addSubview(celula.posterPersonagem)
+       // addSubview(celula)
 
         NSLayoutConstraint.activate([
-            celulaPersonagem.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
-            celulaPersonagem.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
-            celulaPersonagem.topAnchor.constraint(equalTo: topAnchor),
-            celulaPersonagem.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -16),
+            celula.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+            celula.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
+            celula.topAnchor.constraint(equalTo: contentView.topAnchor),
+            celula.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -16),
+            
+            
             
         ])
-        backgroundColor = .clear
-        contentView.backgroundColor = .clear
+     backgroundColor = .clear
+    contentView.backgroundColor = .clear
         
         
     }
